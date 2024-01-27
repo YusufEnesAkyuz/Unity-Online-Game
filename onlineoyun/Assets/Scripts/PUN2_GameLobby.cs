b5d041a603597c9c61387b15363feef1f7728d94
YusufEnesAkyuz <yusufenesakyuz9@gmail.com>
2024-01-27 19:37:54 +0300

    Unity Online oyun
    Unity de photon 2 ile multiplayer geliştirdiğim oyun

---
+++ onlineoyun/Assets/Scripts/PUN2_GameLobby.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PUN2_GameLobby : MonoBehaviourPunCallbacks
+{
+
+   
+    string playerName = "Player 1";
+    
+    string gameVersion = "0.9";
+    
+    List<RoomInfo> createdRooms = new List<RoomInfo>();
+    
+    string roomName = "Room 1";
+    Vector2 roomListScroll = Vector2.zero;
+    bool joiningRoom = false;
+
+    void Start()
+    {
+        
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        if (!PhotonNetwork.IsConnected)
+        {
+           
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = gameVersion;
+            
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnFailedToConnectToPhoton. StatusCode: " + cause.ToString() + " ServerAddress: " + PhotonNetwork.ServerAddress);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+        
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("We have received the Room list");
+        
+        createdRooms = roomList;
+    }
+
+    void OnGUI()
+    {
+        GUI.Window(0, new Rect(Screen.width / 2 - 450, Screen.height / 2 - 200, 900, 400), LobbyWindow, "Lobby");
+    }
+
+    void LobbyWindow(int index)
+    {
+        
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Status: " + PhotonNetwork.NetworkClientState);
+
+        if (joiningRoom || !PhotonNetwork.IsConnected || PhotonNetwork.NetworkClientState != ClientState.JoinedLobby)
+        {
+            GUI.enabled = false;
+        }
+
+        GUILayout.FlexibleSpace();
+
+       
+        roomName = GUILayout.TextField(roomName, GUILayout.Width(250));
+
+        if (GUILayout.Button("Create Room", GUILayout.Width(125)))
+        {
+            if (roomName != "")
+            {
+                joiningRoom = true;
+
+                RoomOptions roomOptions = new RoomOptions();
+                roomOptions.IsOpen = true;
+                roomOptions.IsVisible = true;
+                roomOptions.MaxPlayers = (byte)10; 
+
+                PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+       
+        roomListScroll = GUILayout.BeginScrollView(roomListScroll, true, true);
+
+        if (createdRooms.Count == 0)
+        {
+            GUILayout.Label("No Rooms were created yet...");
+        }
+        else
+        {
+            for (int i = 0; i < createdRooms.Count; i++)
+            {
+                GUILayout.BeginHorizontal("box");
+                GUILayout.Label(createdRooms[i].Name, GUILayout.Width(400));
+                GUILayout.Label(createdRooms[i].PlayerCount + "/" + createdRooms[i].MaxPlayers);
+
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Join Room"))
+                {
+                    joiningRoom = true;
+
+              
+                    PhotonNetwork.NickName = playerName;
+
+                 
+                    PhotonNetwork.JoinRoom(createdRooms[i].Name);
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.EndScrollView();
+
+ 
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Player Name: ", GUILayout.Width(85));
+      
+        playerName = GUILayout.TextField(playerName, GUILayout.Width(250));
+
+        GUILayout.FlexibleSpace();
+
+        GUI.enabled = (PhotonNetwork.NetworkClientState == ClientState.JoinedLobby || PhotonNetwork.NetworkClientState == ClientState.Disconnected) && !joiningRoom;
+        if (GUILayout.Button("Refresh", GUILayout.Width(100)))
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+               
+                PhotonNetwork.JoinLobby(TypedLobby.Default);
+            }
+            else
+            {
+               
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        if (joiningRoom)
+        {
+            GUI.enabled = true;
+            GUI.Label(new Rect(900 / 2 - 50, 400 / 2 - 10, 100, 20), "Connecting...");
+        }
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
+        joiningRoom = false;
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRoomFailed got called. This can happen if the room is not existing or full or closed.");
+        joiningRoom = false;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed got called. This can happen if the room is not existing or full or closed.");
+        joiningRoom = false;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+      
+        PhotonNetwork.NickName = playerName;
+        
+        PhotonNetwork.LoadLevel("GameLevel");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+    }
+}
